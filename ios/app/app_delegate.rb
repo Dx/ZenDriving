c2fc938cf931f791de73441a03837931daf62b0f
David Cruz <dequis@gmail.com>
2012-08-18 01:34:08 -0500

    Con esto ya no pago los 500 varitos, animado como se llena el carrito

--- ios/app/app_delegate.rb
+++ ios/app/app_delegate.rb
@@ -3,14 +3,7 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     
     first_controller = DriverController.alloc.initWithNibName(nil, bundle:nil)
-    # nav_controller = UINavigationController.alloc.initWithRootViewController(first_controller)
-
-    # driver_controller = DriverController.alloc.initWithNibName(nil, bundle:nil)
-    # driver_controller.title = "Drive"
-
-    # tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
-    # tab_controller.viewControllers = [nav_controller, driver_controller]
-
+    
     @window.rootViewController = first_controller
 
     @window.rootViewController.wantsFullScreenLayout = true
--- ios/app/controllers/driver_controller.rb
+++ ios/app/controllers/driver_controller.rb
@@ -4,13 +4,50 @@ class DriverController < UIViewController
     @temp_score = 0
     @total_score = 0
     @temp_distance = 0
-    self.view.backgroundColor = UIColor.alloc.initWithPatternImage(UIImage.imageNamed("play.png"))
+
+    self.view.backgroundColor = UIColor.blackColor
+
+    @level_view = UIView.alloc.initWithFrame [[0, 215], [400, 400]]
+    @level_view.backgroundColor = UIColor.yellowColor
+
+    self.view.addSubview(@level_view)
+
+    @score_view = UIView.alloc.initWithFrame [[0, 215], [400, 400]]
+    @score_view.backgroundColor = UIColor.blueColor
+
+    self.view.addSubview(@score_view)
+
+    @car_view = UIView.alloc.initWithFrame [[0, 0], [480, 320]]
+    @car_view.backgroundColor = UIColor.alloc.initWithPatternImage(UIImage.imageNamed("play1.png"))
+
+    self.view.addSubview(@car_view)
+
+
 
     configure_ui
     suscribe_to_events
 
+    view
+
     @engine = Engine.new
     @engine.start
+  end
+
+  def animate_to_next_point(view, percentage)
+    
+    position = (((100-percentage) * 110) / 100) + 105
+
+    UIView.animateWithDuration(0.3, delay: 0.3, options: UIViewAnimationOptionCurveLinear, 
+      animations:lambda{
+          view.frame = [[0,position], [400, 400]]
+        },
+      completion:lambda{|finished| 
+          # self.animate_to_next_point
+        }
+    )
+    # 215 = 0 105 = 100
+    #  110 = 100
+    #    ? = x
   end
 
   def touchesEnded(touches, withEvent:event)
@@ -33,21 +70,21 @@ class DriverController < UIViewController
   def suscribe_to_events
     @foreground_observer = App.notification_center.observe "notificationLocator" do |notification|
 
-      @movement_label.text = notification.object.distance.to_i.to_s
-
       @temp_score += notification.object.distance.to_i
 
       if @temp_distance + notification.object.distance.to_i <= 100
         @temp_distance += notification.object.distance.to_i
       else        
-        @total_score += @temp_score
+        @total_score += (@temp_score > 100) ? 100 : @temp_score
+        animate_to_next_point @level_view, 100
         @temp_distance = 0
         @temp_score = 0
       end
 
       @level_label.text = @temp_score.to_s
-      @distance_label.text = @temp_distance.to_s
       @score_label.text = @total_score.to_s
+      animate_to_next_point @level_view, @temp_distance
+      animate_to_next_point @score_view, @temp_score
 
     end
 
@@ -60,8 +97,10 @@ class DriverController < UIViewController
       end
 
       @level_label.text = @temp_score.to_s
-      @distance_label.text = @temp_distance.to_s
       @score_label.text = @total_score.to_s
+
+      animate_to_next_point @level_view, @temp_distance
+      animate_to_next_point @score_view, @temp_score
 
       @accelerate_label.text = notification.object.level.to_s
 
@@ -78,94 +117,31 @@ class DriverController < UIViewController
   
   def configure_ui
 
-    @accelerate_title = UILabel.new
-    @accelerate_title.font = UIFont.systemFontOfSize(20)
-    @accelerate_title.text = 'Movimiento'
-    @accelerate_title.textAlignment = UITextAlignmentCenter 
-    @accelerate_title.textColor = UIColor.redColor
-    @accelerate_title.backgroundColor = UIColor.clearColor
-    @accelerate_title.frame = [[20, 20], [150, 30]]
-    self.view.addSubview(@accelerate_title)
-
-    @movement_title = UILabel.new
-    @movement_title.font = UIFont.systemFontOfSize(20)
-    @movement_title.text = 'Nuevos metros'
-    @movement_title.textAlignment = UITextAlignmentCenter
-    @movement_title.textColor = UIColor.redColor
-    @movement_title.backgroundColor = UIColor.clearColor
-    @movement_title.frame = [[20, 50], [150, 30]]
-    self.view.addSubview(@movement_title)
-
-    @distance_title = UILabel.new
-    @distance_title.font = UIFont.systemFontOfSize(20)
-    @distance_title.text = 'Medida 100m'
-    @distance_title.textAlignment = UITextAlignmentCenter
-    @distance_title.textColor = UIColor.redColor
-    @distance_title.backgroundColor = UIColor.clearColor
-    @distance_title.frame = [[20, 80], [150, 30]]
-    self.view.addSubview(@distance_title)
-
-    @level_title = UILabel.new
-    @level_title.font = UIFont.systemFontOfSize(20)
-    @level_title.text = 'Score 100m'
-    @level_title.textAlignment = UITextAlignmentCenter
-    @level_title.textColor = UIColor.redColor
-    @level_title.backgroundColor = UIColor.clearColor
-    @level_title.frame = [[20, 110], [150, 30]]
-    self.view.addSubview(@level_title)
-
-    @score_title = UILabel.new
-    @score_title.font = UIFont.systemFontOfSize(20)
-    @score_title.text = 'SCORE TOTAL'
-    @score_title.textAlignment = UITextAlignmentCenter
-    @score_title.textColor = UIColor.blackColor
-    @score_title.backgroundColor = UIColor.clearColor
-    @score_title.frame = [[20, 140], [150, 30]]
-    self.view.addSubview(@score_title)
-
     @accelerate_label = UILabel.new
-    @accelerate_label.font = UIFont.systemFontOfSize(25)
-    @accelerate_label.text = 'ACE'
+    @accelerate_label.font = UIFont.systemFontOfSize(15)
+    @accelerate_label.text = 'Acel'
     @accelerate_label.textAlignment = UITextAlignmentCenter 
-    @accelerate_label.textColor = UIColor.redColor
+    @accelerate_label.textColor = UIColor.whiteColor
     @accelerate_label.backgroundColor = UIColor.clearColor
-    @accelerate_label.frame = [[180, 20], [150, 30]]
+    @accelerate_label.frame = [[20, 20], [150, 30]]
     self.view.addSubview(@accelerate_label)
 
-    @movement_label = UILabel.new
-    @movement_label.font = UIFont.systemFontOfSize(25)
-    @movement_label.text = 'MOVE'
-    @movement_label.textAlignment = UITextAlignmentCenter
-    @movement_label.textColor = UIColor.redColor
-    @movement_label.backgroundColor = UIColor.clearColor
-    @movement_label.frame = [[180, 50], [150, 30]]
-    self.view.addSubview(@movement_label)
-
-    @distance_label = UILabel.new
-    @distance_label.font = UIFont.systemFontOfSize(25)
-    @distance_label.text = 'DIST'
-    @distance_label.textAlignment = UITextAlignmentCenter
-    @distance_label.textColor = UIColor.redColor
-    @distance_label.backgroundColor = UIColor.clearColor
-    @distance_label.frame = [[180, 80], [150, 30]]
-    self.view.addSubview(@distance_label)
-
     @level_label = UILabel.new
-    @level_label.font = UIFont.systemFontOfSize(25)
-    @level_label.text = 'LEVEL'
+    @level_label.font = UIFont.systemFontOfSize(35)
+    @level_label.text = '0'
     @level_label.textAlignment = UITextAlignmentCenter
-    @level_label.textColor = UIColor.redColor
+    @level_label.textColor = UIColor.whiteColor
     @level_label.backgroundColor = UIColor.clearColor
-    @level_label.frame = [[180, 110], [150, 30]]
+    @level_label.frame = [[160, 150], [150, 30]]
     self.view.addSubview(@level_label)
 
     @score_label = UILabel.new
-    @score_label.font = UIFont.systemFontOfSize(25)
-    @score_label.text = 'SCORE'
+    @score_label.font = UIFont.systemFontOfSize(35)
+    @score_label.text = '0'
     @score_label.textAlignment = UITextAlignmentCenter
-    @score_label.textColor = UIColor.blackColor
+    @score_label.textColor = UIColor.blueColor
     @score_label.backgroundColor = UIColor.clearColor
-    @score_label.frame = [[180, 140], [150, 30]]
+    @score_label.frame = [[160, 250], [150, 30]]
     self.view.addSubview(@score_label)
   end
 end
